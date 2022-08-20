d562772f90f06cb509de1b049ca6a219b209d872
jake <manzonjake15@gmail.com>
2022-08-20 23:00:38 +0800

    initialized firebase app

--- born2blegend/src/app/app.module.ts
+++ born2blegend/src/app/app.module.ts
@@ -4,7 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireFunctionsModule } from '@angular/fire/compat/functions';
+import { AngularFireMessagingModule } from '@angular/fire/compat/messaging';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,9 +19,18 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+    AngularFireFunctionsModule,
+    AngularFireMessagingModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
+  
--- born2blegend/src/environments/environment.prod.ts
+++ born2blegend/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCUOX-PiclYwuDULVvjA7c91DYKQ_z2ioI",
+    authDomain: "born2blegend-7172022.firebaseapp.com",
+    databaseURL: "https://born2blegend-7172022-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "born2blegend-7172022",
+    storageBucket: "born2blegend-7172022.appspot.com",
+    }
+}
